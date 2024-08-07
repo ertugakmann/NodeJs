38330c55de20ff5229f4be07de420b19ec8ef8d7
ertugakmann <134059022+ertugakmann@users.noreply.github.com>
2024-08-07 18:41:18 +0300

    Refactored server code and add template rendering for overview page

--- index.js
+++ index.js
@@ -29,24 +29,65 @@ const url = require("url");
 
 /////////////// SERVER ///////////////
 
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
+
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-
   const pathName = req.url;
 
+  //Overview Page
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the OVERVIEW");
-  } else if (pathName === "/product") {
-    res.end("This is the PRODUCT");
-  } else if (pathName == "/api") {
     res.writeHead(200, {
       "Content-type": "application/json",
     });
-    res.end(data);
-  } else {
+
+    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el));
+    console.log(cardsHtml);
+    res.end(tempOverview);
+  }
+
+  //Product Page
+  else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  }
+
+  // API
+  else if (pathName == "/api") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    res.end(dataObj);
+  }
+
+  // Not Found
+  else {
     res.writeHead(404, {
       "Content-type": "text/html",
       "my-own-header": "hello-world",
